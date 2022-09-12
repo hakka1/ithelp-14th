3d2022121af9e9705e48917c2b67684efe7ee91b
Tengi Huang <tengi.huang@gmail.com>
2022-09-12 18:47:54 +0800

    feat: implement example of day3

---
+++ examples/example-day3/app/src/main/java/com/hakka1/ithelp/example/day2/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.hakka1.ithelp.example.day2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange = Color(0xFFFF9800)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
